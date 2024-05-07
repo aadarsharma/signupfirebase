0f2638126d750f75afe00ddbea18adfceb2bc0cb
Aadarsh Sharma <96333496+aadarsharma@users.noreply.github.com>
2024-05-07 15:14:32 +0530

    Update and rename config.js.examole to config.js

--- config.js
+++ config.js
@@ -7,13 +7,13 @@ import { getAnalytics } from "firebase/analytics";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyBp0fFLxw6EMnDGoNoGKRq5cq-_R5-WMnMa",
-  authDomain: "authentication-dcca0.firebaseapp.coma",
-  projectId: "authentication-dcca0a",
-  storageBucket: "authentication-dcca0.appspot.coma",
-  messagingSenderId: "821989640671a",
-  appId: "1:821989640671:web:056069850422cc94885c6aa",
-  measurementId: "G-KH1WM0Z9EHa"
+  apiKey: "AIzaSyBp0fFLxw6EMnDGoNoGKRq5cq-_R5-WMnM",
+  authDomain: "authentication-dcca0.firebaseapp.com",
+  projectId: "authentication-dcca0",
+  storageBucket: "authentication-dcca0.appspot.com",
+  messagingSenderId: "821989640671",
+  appId: "1:821989640671:web:056069850422cc94885c6a",
+  measurementId: "G-KH1WM0Z9EH"
 };
 
 // Initialize Firebase
